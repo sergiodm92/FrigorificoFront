7656eca32481ee9e79c85acd2c8ccaf123fbe90f
sergiodm92 <97261829+sergiodm92@users.noreply.github.com>
2022-09-05 20:32:41 -0300

    combios en json

--- client/src/Redux/Reducer/Reducer.js
+++ client/src/Redux/Reducer/Reducer.js
@@ -3,6 +3,9 @@ const initialState = {}
 const rootReducer = (state = initialState, action) => {
     const { type, payload } = action;
     switch (action.type) {
+
+
+        
         default:
         return state;
     }
