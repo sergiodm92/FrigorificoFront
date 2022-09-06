64f7af1d23909be5e99b4551bbb754dea6041bcb
sergiodm92 <97261829+sergiodm92@users.noreply.github.com>
2022-09-06 19:13:02 -0300

    as

--- client/src/Pages/Compra/Detalle_compra.jsx
+++ client/src/Pages/Compra/Detalle_compra.jsx
@@ -3,15 +3,17 @@ import NavBar from "../../Components/Navbar/Navbar"
 import TableCompra from "../../Components/Details/Detalle_Compra"
 import StyleDetalleCompra from './StyleDetalleCompras.module.scss'
 
-export default function Detalle_Compra(){
-    
+export default function Detalle_Compra(){//{id_compra}
+const id_c=2
     return(
         <div className={StyleDetalleCompra.ConteinerCompras}>
         <NavBar
         title={"Detalle"}
         />
         <div className={StyleDetalleCompra.TableCompras}>
-        <TableCompra></TableCompra>
+        <TableCompra
+        id_c={id_c}
+        />
         </div>
         </div>
 
