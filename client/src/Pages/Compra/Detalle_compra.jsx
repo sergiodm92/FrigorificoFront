4bfc78aed228615bbdaaaeb673afd0a1de6a2da2
sergiodm92 <97261829+sergiodm92@users.noreply.github.com>
2022-09-06 17:49:05 -0300

    compra

--- client/src/Pages/Compra/Detalle_compra.jsx
+++ client/src/Pages/Compra/Detalle_compra.jsx
@@ -1,17 +1,18 @@
 import React from "react"
-
-
+import NavBar from "../../Components/Navbar/Navbar"
+import TableCompra from "../../Components/Details/Detalle_Compra"
+import StyleDetalleCompra from './StyleDetalleCompras.module.scss'
 
 export default function Detalle_Compra(){
-
+    
     return(
-        <div>
-
-
-
-
-
-
+        <div className={StyleDetalleCompra.ConteinerCompras}>
+        <NavBar
+        title={"Detalle"}
+        />
+        <div className={StyleDetalleCompra.TableCompras}>
+        <TableCompra></TableCompra>
+        </div>
         </div>
 
     )
