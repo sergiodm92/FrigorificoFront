540a33e46a5ada8f841b31a91daa377a5285d68a
Mariana Rionda <maririonda@gmail.com>
2022-09-20 13:05:46 -0300

    Nueva Faena y Nueva Venta listas

--- client/src/Pages/Faenas/Form_Faena.jsx
+++ client/src/Pages/Faenas/Form_Faena.jsx
@@ -1,15 +1,14 @@
-import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
+import React, { useState, useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { useNavigate } from "react-router";
 import swal from "sweetalert";
-import ButtonNew from "../../Components/Buttons/ButtonNew/ButtonNew";
 import ShortButton from "../../Components/Buttons/Button_Short/Button_Short";
-
-
-import NavBar from '../../Components/Navbar/Navbar'
-
+import ButtonNew from "../../Components/Buttons/ButtonNew/ButtonNew";
+import NavBar from '../../Components/Navbar/Navbar';
 import styleFormF from './Form_Faena.module.scss';
+import CardReses from "../../Components/Cards/CardReses/CardReses";
 
+//Form Faena
 const formF = {
     fecha: '',
     frigorifico: '',
@@ -17,42 +16,56 @@ const formF = {
     proveedor: '',
     detalle:[],
 };
+//Form para cargar las reses del detalle de Faena
 const formComF = {
     correlativo: '',
     categoria: '',
     kg: ''
 };
 
-const frigorificos = ["Natilla", "otro"]
+//Arrays para los selects
+const frigorificos = ["Natilla", "El Hueco"]
 const proveedores = ["Puchulo", "Stopa", "Castillo", "Dib", "Dulio Text", "C Walter"]
 const categorias = ["Vaquillon", "Novillo", "Vaca", "Toro"]
 
-//validaciones
+//validaciones form Faena
 export const validate = (faena) => {
     let error = {};
-
     if (!faena.fecha) error.fecha = "Falta fecha";
     else if (!/^([0-2][0-9]|3[0-1])(\/|-)(0[1-9]|1[0-2])\2(\d{4})$/.test(faena.fecha)) error.fecha = "Fecha incorrecta";
     if (!faena.tropa) error.tropa = "Falta tropa";
     else if (!/^([0-9])*$/.test(faena.tropa)) error.tropa = "Tropa debe ser un número";
-    if (!faena.correlativo) error.correlativo = "Falta correlativo";
-    else if (!/^([0-9])*$/.test(faena.correlativo)) error.correlativo = "Correlativo debe ser un número";
-    if (!faena.categoria) error.categoria = "Falta categoría";
-    if (!faena.kg) error.kg = "Falta kg";
-    else if (!/^([0-9])*$/.test(faena.kg)) error.kg = "kg debe ser un número";
+    if (faena.detalle.length<1) error.detalle = "Falta detalle";
     return error;
 };
+
+//validaciones de reses
+export const validate2 = (res) => {
+    let error2 = {};
+    if (!res.correlativo) error2.correlativo = "Falta correlativo";
+    else if (!/^([0-9])*$/.test(res.correlativo)) error2.correlativo = "Correlativo debe ser un número";
+    if (!res.categoria) error2.categoria = "Falta categoría";
+    if (!res.kg) error2.kg = "Falta kg";
+    else if (!/^([0-9])*$/.test(res.kg)) error2.kg = "kg debe ser un número";
+    return error2;
+};
+
 
 const Form_Faena = () => {
 
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
+    //Estados locales
     const [form, setForm] = useState(formF);
     const [formCF, setFormCF] = useState(formComF)
     const [error, setError] = useState({});
+    const [error2, setError2] = useState({});
 
-    const handleChange = (e) => {
+
+    //handleChange de la faena completa
+    const handleChange = (e) => {  
+        e.preventDefault();
         setError(
         validate({
             ...form,
@@ -65,20 +78,30 @@ const Form_Faena = () => {
         });
     };
 
-    const handleSubmitRes = () => {
-        if(
-        !error.categoria && form.categoria &&
-        !error.kg && form.kg &&
-        !error.correlativo && form.correlativo
-        ){
-        // dispatch(postFaena(form))
-        swal({
-            title: "Nueva Res",
-            text: " Res cargada correctamente",
-            icon: "success",
-            button: "ok",
+    //handleChange de reses
+    const handleChangeCF = (e) => { 
+        setError2(
+        validate2({
+            ...formCF,
+            [e.target.name]: e.target.value,
         })
-        setForm(formComF);
+        );
+        setFormCF({
+            ...formCF,
+            [e.target.name]: e.target.value,
+        });
+    };
+
+    //handleSubmit de las reses
+    const handleSubmitRes = (e) => {   
+        e.preventDefault();
+        if(
+        !error2.categoria && formCF.categoria &&
+        !error2.kg && formCF.kg &&
+        !error2.correlativo && formCF.correlativo
+        ){
+            form.detalle.push(formCF)
+            setFormCF(formComF);
         }
         else {
             swal({
@@ -90,19 +113,22 @@ const Form_Faena = () => {
         }
     };
 
-    const handleSubmit = () => {
+    //handleSubmit de la faena completa
+    const handleSubmit = (e) => {
+        e.preventDefault();
         if(
         !error.fecha && form.fecha &&
         !error.tropa && form.tropa
         ){
-        // dispatch(postFaena(form))
-        swal({
-            title: "Nueva Faena",
-            text: " Faena cargada correctamente",
-            icon: "success",
-            button: "ok",
-        })
-        setForm(formF);
+            // dispatch(postFaena(form))
+            console.log(form)
+            swal({
+                title: "Nueva Faena",
+                text: " Faena cargada correctamente",
+                icon: "success",
+                button: "ok",
+            })
+            setForm(formF);
         }
         else {
             swal({
@@ -114,29 +140,40 @@ const Form_Faena = () => {
         }
     };
 
+    //Select de frigoríficos
     function handleSelectFr(e) {
         setForm({
             ...form,
-            frigorifico: [...form.frigorifico, e.target.value ]
+            frigorifico:  e.target.value 
         })
     }
+    //Select de proveedores
     function handleSelectPr(e) {
         setForm({
             ...form,
-            proveedor: [...form.proveedor, e.target.value ]
+            proveedor:e.target.value 
         })
     }
+    //Select de categorías
     function handleSelect(e) {
-        setForm({
+        setFormCF({
             ...formCF,
-            categoria: [...formCF.categoria, e.target.value ]
+            categoria:  e.target.value 
         })
     }
 
+    //ir faenas para ver el detalle
     const handleDet = () => {
         navigate("/Faenas")
-
     };
+
+    //funcion para eliminar reses del detalle
+    const handleDelete = (e)=>{
+        setForm({
+            ...form,
+            detalle: form.detalle.filter(d => d !== e)
+        })
+    }
 
     return (
         <div className={styleFormF.wallpaper}>
@@ -193,15 +230,17 @@ const Form_Faena = () => {
                             }
                         </select>
                     </div>
+
+                    {/*----------------Carga del detalle---------------------*/}
                     <div className={styleFormF.formItem2}>
                         <div className={styleFormF.item}>
                             <h5 className={styleFormF.title}>Correlativo: </h5>
                             <input
                                 type="text"
-                                value={form.correlativo}
+                                value={formCF.correlativo}
                                 id="correlativo"
                                 name="correlativo"
-                                onChange={handleChange}
+                                onChange={handleChangeCF}
                                 placeholder="0000"
                                 className={styleFormF.size2}
                             />
@@ -219,10 +258,10 @@ const Form_Faena = () => {
                                 <h5 className={styleFormF.title}>kg </h5>
                                 <input
                                     type="text"
-                                    value={form.kg}
+                                    value={formCF.kg}
                                     id="kg"
                                     name="kg"
-                                    onChange={handleChange}
+                                    onChange={handleChangeCF}
                                     placeholder="00"
                                     className={styleFormF.size2}
                                 />
@@ -231,11 +270,26 @@ const Form_Faena = () => {
                     </div>
                     <div className={styleFormF.button}>
                         <ButtonNew
+                            onClick={handleSubmitRes}
                             style={"rigth"}
                             icon={"rigth"}
-                            onClick={handleSubmitRes}
                         />
                     </div>
+                    {/*-----------------------------------------------------------*/}
+                    
+                    {form.detalle.length ?
+                        form.detalle.map((e)=>{
+                            return(
+                                <CardReses
+                                    correlativo={e.correlativo}
+                                    categoria={e.categoria}
+                                    kg={e.kg}
+                                    onClick={()=> handleDelete(e)}
+                                />
+                            )
+                        })
+                        :null
+                    }
                     <div className={styleFormF.formItem}>
                         <div>
                             <h5 className={styleFormF.title}>Costo Faena/kg: </h5>
