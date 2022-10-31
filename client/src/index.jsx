4448e7ea0bb680cf07d72a7a54a321e4f400f72f
Sergio Andrés David Maioli <97261829+sergiodm92@users.noreply.github.com>
2022-10-31 14:49:51 -0300

    Update index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,8 +7,8 @@ import store from './Redux/Store/Store.js';
 import { Provider } from 'react-redux';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3001'
-// axios.defaults.baseURL =  'https://donalberto.herokuapp.com/' || 'http://localhost:3001'
+// axios.defaults.baseURL = 'http://localhost:3001'
+axios.defaults.baseURL =  'https://donalberto.herokuapp.com/' 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
