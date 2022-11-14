cb7d35a66eafddd364ce9c32c86a897cb08a7027
sergiodm92 <116969294+FincadonAlberto@users.noreply.github.com>
2022-11-14 16:17:35 -0300

    asd

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -8,7 +8,7 @@ import { Provider } from 'react-redux';
 import axios from 'axios';
 
 // axios.defaults.baseURL = 'http://localhost:3001'
-axios.defaults.baseURL =  'https://donalberto.herokuapp.com/' || 'http://localhost:3001'
+axios.defaults.baseURL =  'https://donalberto.herokuapp.com/' 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Provider store={store}>
